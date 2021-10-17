0ac9bde96696a803ec26651762a251970425260f
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-10-17 13:20:58 +0300

    refactor fetch-countries-api function

--- src/js/fetch-countries-api.js
+++ src/js/fetch-countries-api.js
@@ -1,42 +1,43 @@
 import API from './fetchCountries';
-const debounce = require('lodash.debounce');
-import { alert, notice, info, success, error, defaultModules } from '@pnotify/core';
+import getRefs from './refs';
+import { error, defaultModules } from '@pnotify/core';
 import * as PNotifyDesktop from '@pnotify/desktop';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-import getRefs from './refs';
 import countryCardTmpl from '../templates/country-card.hbs';
 import countriesListTmpl from '../templates/countries-list.hbs';
 
+const debounce = require('lodash.debounce');
 const refs = getRefs();
+const onFetchCountries = debounce(() => {
+  if (refs.searchQuery.value.length === 0) {
+    return;
+  }
+  API.fetchCountries(refs.searchQuery.value).then(renderCountries).catch(onFetchError);
+}, 500);
 
-refs.searchQuery.addEventListener(
-  'input',
-  debounce(() => {
-    if (refs.searchQuery.value.length === 0) {
-      return;
-    }
-    API.fetchCountries(refs.searchQuery.value).then(renderCountries).catch(onFetchError);
-  }, 1000),
-);
+refs.searchQuery.addEventListener('input', onFetchCountries);
 
 function renderCountries(res) {
-  if (res.length === 1) {
-    const markup = countryCardTmpl(res);
-    refs.cardContainer.innerHTML = markup;
+  if (res.status === 404) {
+    onFetchError();
+  } else if (res.length === 1) {
+    refs.cardContainer.innerHTML = countryCardTmpl(res);
   } else if (res.length > 10) {
-    error({
-      text: 'Too many matches found. Please enter more specific query!',
-      modules: new Map([...defaultModules, [PNotifyDesktop, {}]]),
-    });
-    refs.searchQuery.value = '';
+    renderError('Too many matches found. Please enter more specific query!');
   } else {
-    const listMarkup = countriesListTmpl(res);
-    refs.cardContainer.innerHTML = listMarkup;
+    refs.cardContainer.innerHTML = countriesListTmpl(res);
   }
 }
 
-function onFetchError(error) {
-  alert('Something went wrong, your country was not found!');
-  refs.searchQuery.value = '';
+function onFetchError() {
+  refs.cardContainer.innerHTML = '';
+  renderError('Something went wrong, your country was not found!');
+}
+
+function renderError(errText) {
+  return error({
+    text: errText,
+    modules: new Map([...defaultModules, [PNotifyDesktop, {}]]),
+  });
 }

e56e824534b415b1c411b473a08d69bb902d85d4
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-10-17 15:43:40 +0300

    fetchCountries functoin update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,12 @@ const BASE_URL = 'https://restcountries.com/v2';
 
 function fetchCountries(searchQuery) {
   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => {
-    return response.json();
+    {
+      if (response.ok) {
+        return response.json();
+      }
+    }
+    throw new Error('Not 2xx response');
   });
 }
 

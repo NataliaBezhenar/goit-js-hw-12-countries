094efd3f8d5f9cb5d7e0c18bfb6e1bf74fdaa24c
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-10-17 10:45:54 +0300

    renderCountries function

--- src/js/fetch-countries-api.js
+++ src/js/fetch-countries-api.js
@@ -4,17 +4,15 @@ import { alert, notice, info, success, error, defaultModules } from '@pnotify/co
 import * as PNotifyDesktop from '@pnotify/desktop';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-import getRefs from "./refs";
-import countryCardTmpl from "../templates/country-card.hbs";
-
+import getRefs from './refs';
+import countryCardTmpl from '../templates/country-card.hbs';
+import countriesListTmpl from '../templates/countries-list.hbs';
 
 const refs = getRefs();
-
 
 refs.searchQuery.addEventListener(
   'input',
   debounce(() => {
-
     if (refs.searchQuery.value.length === 0) {
       return;
     }
@@ -23,29 +21,22 @@ refs.searchQuery.addEventListener(
 );
 
 function renderCountries(res) {
-if(res.length===1){
-  const markup = countryCardTmpl(res);
-  refs.cardContainer.innerHTML = markup;
-
-}
-
-
-
-
-  refs.output.textContent = res[0].name;
-  if (res.length > 10) {
-    console.log('res length is ' + res.length);
-
+  if (res.length === 1) {
+    const markup = countryCardTmpl(res);
+    refs.cardContainer.innerHTML = markup;
+  } else if (res.length > 10) {
     error({
       text: 'Too many matches found. Please enter more specific query!',
       modules: new Map([...defaultModules, [PNotifyDesktop, {}]]),
     });
-    
-    refs.output.textContent = '';
+    refs.searchQuery.value = '';
+  } else {
+    const listMarkup = countriesListTmpl(res);
+    refs.cardContainer.innerHTML = listMarkup;
   }
 }
 
 function onFetchError(error) {
   alert('Something went wrong, your country was not found!');
-  refs.searchQuery.value='';
+  refs.searchQuery.value = '';
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,6 @@
 export default function getRefs() {
   return {
     searchQuery: document.querySelector('input'),
-    output: document.querySelector('.output__text'),
     cardContainer: document.querySelector('.js-card-container'),
   };
 }

339f6d683214a05c77eb5e76fd4fdefac19f3157
NataliaBezhenar <natalia.bezhenar@gmail.com>
2022-07-16 15:51:53 +0000

    Deploying to gh-pages from @ NataliaBezhenar/goit-js-hw-12-countries@5a9538133b0acb25a65208558bd0dea8ac677e4d 🚀

--- src.64f02afd.js
+++ src.64f02afd.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../../img/globus.jpg":[["globus.7728bdcb.jpg","Jh1i"],"Jh1i"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-12-countries/src.eb53dbd6.js.map
+//# sourceMappingURL=/goit-js-hw-12-countries/src.64f02afd.js.map
